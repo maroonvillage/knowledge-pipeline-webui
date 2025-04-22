9847816dcbcda43d998f714b7bc24628621bd262
Cole P <maroonyear0@gmail.com>
2025-04-22 16:04:18 -0700

    Add security group configuration for HTTP and SSH access

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -2,6 +2,7 @@
 resource "aws_instance" "app_server" {
   ami           = "ami-0ce45259f491c3d4f"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sg.id] #This is the key!
 
   tags = {
     Name = "ExampleAppServerInstance"
---
+++ terraform/ec2/security_group.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "sg" {
+  name        = "pdfdocintel_allow_http_ssh"
+  description = "Allow HTTP and SSH traffic"
+  vpc_id      = "vpc-00230279b21071217"  # Replace with your VPC ID
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["192.168.87.21/32"]  #  Make sure to narrow this down!
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["192.168.87.21/32"] # Make sure to narrow this down!
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
