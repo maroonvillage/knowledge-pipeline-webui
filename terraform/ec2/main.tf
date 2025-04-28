12a99b346bf1d8365efbb6440a15a5ceb3b20aac
Cole P <maroonyear0@gmail.com>
2025-04-28 16:56:18 -0700

    Update .gitignore to exclude Terraform state files and enhance Ansible deployment playbook for ECR image management

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -3,8 +3,8 @@ data "aws_key_pair" "existing" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0ce45259f491c3d4f"
-  instance_type = "t2.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type # Example instance type, adjust as needed
   key_name               = data.aws_key_pair.existing.key_name
   vpc_security_group_ids = [aws_security_group.sg.id] #This is the key!
   iam_instance_profile = var.ec2_instance_profile_var
